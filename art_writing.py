fd149f3cb9947ac3b5e01d9dbe68a93c48a8a9c5
celebi19 <hakan_socialmedia@hotmail.com>
2022-10-01 21:43:25 -0700

    new commit

---
+++ art_writing.py
@@ -0,0 +1,24 @@
+
+my_string = 'hakan'
+x = 0
+
+for i in my_string:
+    x = x + 1
+    print(my_string[0:x])
+    
+for i in my_string:
+    x = x - 1
+    print(my_string[0:x])
+    
+    
+    
+my_string = 'kilic'
+x = 0
+
+for i in my_string:
+    x = x + 1
+    print(my_string [0:x])
+    
+for i in my_string:
+    x = x - 1
+    print(my_string[0:x])
