1bcaf91801a5d0ce37bd068135c1eff06b56314d
celebi19 <hakan_socialmedia@hotmail.com>
2022-10-30 20:53:56 -0700

    new files

---
+++ wordart2.py
@@ -0,0 +1,7 @@
+# import module
+from art import *
+  
+Filename = tsave(
+    "HAKAN", filename= "Hakanwordart_tsave().txt")
+
+# more samples on  https://www.geeksforgeeks.org/art-module-in-python/
